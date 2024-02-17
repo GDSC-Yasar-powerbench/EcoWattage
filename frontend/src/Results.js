e815a5bb00a1605909e52d6b32b5bb1d7ce8acab
sarpsenturk <sarpsenturk38@gmail.com>
2024-02-18 01:00:04 +0300

    Add pie chart for results

--- frontend/src/Results.js
+++ frontend/src/Results.js
@@ -6,6 +6,7 @@ import TextField from '@mui/material/TextField'
 import InputAdornment from '@mui/material/InputAdornment'
 import CircularProgress from '@mui/material/CircularProgress'
 import Button from '@mui/material/Button'
+import Typography from '@mui/material/Typography';
 
 import { ResponsivePie } from '@nivo/pie'
 
@@ -14,18 +15,18 @@ import { useState } from 'react'
 function Results() {
     const [advanced, setAdvanced] = useState(false)
     const results = {
-        compile_time: 0.98,
-        exec_time: 9.14
+        compile_power: 0.0012,
+        exec_power: 0.0212
     }
 
-    const timeData = [
+    const powerData = () => [
         {
             id: "compile",
-            value: results.compile_time
+            value: results.compile_power
         },
         {
             id: "execution",
-            value: results.exec_time
+            value: results.exec_power
         }
     ]
 
@@ -34,7 +35,7 @@ function Results() {
     }
 
     return (
-        <Box sx={{ padding: "1rem" }}>
+        <Box sx={{ padding: "1rem", display: 'flex', flexDirection: 'column' }}>
             <TextField id='cpu-name' type='text' label='CPU Name' variant='outlined' disabled={advanced} fullWidth />
             <TextField
                 id='cpu-power'
@@ -48,7 +49,18 @@ function Results() {
             />
             <FormControlLabel control={<Checkbox onChange={(event) => { setAdvanced(event.target.checked) }} />} label='Advanced' />
             <Button type='submit' fullWidth variant='contained'>Benchmark</Button>
-            <ResponsivePie data={timeData} />
+            <Box sx={{ height: '400px', display: 'flex' }}>
+                <ResponsivePie
+                    data={powerData()}
+                    margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+                    innerRadius={0.5}
+                />
+            </Box>
+            <Typography component='div'>
+                Compilation consumed <Box sx={{fontWeight: 'bold'}}>{results.compile_power}kWh</Box>
+                Execution consumed <Box sx={{fontWeight: 'bold'}}>{results.exec_power}kWh</Box>
+                In total, consumed <Box sx={{fontWeight: 'bold'}}>{results.compile_power + results.exec_power}kWh</Box>
+            </Typography>
         </Box>
     )
 }
