a1b4ffef4bbc75e26cbda5153cfe8dc2aad67660
sarpsenturk <sarpsenturk38@gmail.com>
2024-02-18 22:52:23 +0300

    Add CPU name autocomplete & advanced mode

--- frontend/src/Benchmark.js
+++ frontend/src/Benchmark.js
@@ -1,7 +1,10 @@
 
 import Box from '@mui/material/Box'
 import TextField from '@mui/material/TextField'
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Checkbox from '@mui/material/Checkbox'
 import InputAdornment from '@mui/material/InputAdornment'
+import Autocomplete from '@mui/material/Autocomplete'
 import Button from '@mui/material/Button'
 
 import Results from './Results'
@@ -12,9 +15,18 @@ import { GetBenchmark } from './API'
 
 function Benchmark({ code }) {
     const [watts, setWatts] = useState(0)
+    const [advanced, setAdvanced] = useState(false)
     const [loading, setLoading] = useState(false)
     const [results, setResults] = useState(null)
     const [errors, setErrors] = useState(null)
+    const [cpus, setCPUs] = useState([
+        {
+            label: 'AMD Ryzen 5 3600X',
+            power: 81.6
+        }
+    ])
+
+
 
     const doBenchmark = async () => {
         try {
@@ -48,6 +60,19 @@ function Benchmark({ code }) {
 
     return (
         <Box sx={{ padding: "1rem", display: 'flex', flexDirection: 'column' }}>
+            <Autocomplete
+                disablePortal
+                id='cpu-name'
+                options={cpus}
+                renderInput={(params) => <TextField {...params} label='CPU Name' />}
+                onChange={(_, value) => setWatts(value ? value.power : 0)}
+                sx={{ marginBottom: '1rem' }}
+                disabled={advanced}
+            />
+            <FormControlLabel
+                label='Advanced'
+                control={<Checkbox onChange={(_, checked) => setAdvanced(checked)} />}
+            />
             <TextField
                 id='cpu-power'
                 label='CPU Power'
@@ -57,6 +82,8 @@ function Benchmark({ code }) {
                 }}
                 fullWidth
                 onChange={(event) => setWatts(event.target.value)}
+                value={watts}
+                disabled={!advanced}
             />
             <Button type='submit' fullWidth variant='contained' onClick={doBenchmark}>Benchmark</Button>
             <Results loading={loading} results={results} errors={errors} />
